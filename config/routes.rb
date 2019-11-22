cea17a833b9e2199d1759e1d7adf5f9bf7a7ee55
Charmaine Lee <charmaine@seeksophie.com>
2019-11-22 16:56:29 +0800

    root path added, remove cocktail index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :cocktails do
+  root to: 'cocktails#index'
+
+
+  resources :cocktails, except: [:index] do
     resources :doses, only: [:index, :new, :create]
   end
   resources :doses, only: [:show, :edit, :update, :destroy]
